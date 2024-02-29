7cab3d7c8513807d709cb2e2b4b18e8200798f5c
Cspetero <onp307304@gmail.com>
2024-02-29 03:19:31 +0300

    all project files

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -52,7 +52,7 @@ export function renderPaymentSummary() {
     </button>
 
     <button id="whatsapp-button" class="whatsapp-button button-secondary">
-      <img class="whatsapp-logo" src="/images/whatsapp_logo.png" alt="">  
+      <img class="whatsapp-logo" src="../../images/whatsapp_logo.png" alt="">  
       Confirm your order here (via WhatsApp)
     </button>
   `;
